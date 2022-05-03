d1411b69462108d90e2a5381a47351395e6ab29b
FernandoGR <gamboa_029@hotmail.com>
2022-05-03 16:45:53 -0500

    W3E3V12

--- app/view/UserView.js
+++ app/view/UserView.js
@@ -6,6 +6,11 @@ class UserView{
     if(payload === null){
       console.log("Error es null")
       return {error: "El payload no existe."}
+    }else if (typeof payload.username === 'string' && typeof payload.name === 'string' && typeof payload.id === 'number') {
+        return UserService.create(payload.id, payload.username, payload.name)
+    }else {
+        console.log("Error tiene un valor nulo");
+        return {error: 'necesitan tener un valor válido'}
     }
   }
 }
--- test/view/UserView.test.js
+++ test/view/UserView.test.js
@@ -6,5 +6,10 @@ describe("Test for UserView", () =>{
         const result = UserView.createUser(payload)
         //https://jestjs.io/docs/using-matcher#strings
         expect(result.error).toMatch(/payload no existe/)
+    });
+    test('2) Return an error object when try to create a new user with a payload with invalid properties', () =>{
+        const payload = {username: null, name: 12, id: "id"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor válido/)
     })
 })
