7c7d375f337aa62f82fd9b8f9db5c4c4b9c66d34
FernandoGR <gamboa_029@hotmail.com>
2022-05-03 16:55:04 -0500

    W3E3V13

--- test/view/UserView.test.js
+++ test/view/UserView.test.js
@@ -11,5 +11,17 @@ describe("Test for UserView", () =>{
         const payload = {username: null, name: 12, id: "id"}
         const result = UserView.createUser(payload)
         expect(result.error).toMatch(/necesitan tener un valor válido/)
+    });
+    test('3) Return an errror object when try to create a new user with a payload with missing properties', () =>{
+        const payload = {username: "Username"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor válido/)
+    });
+    test('4) Create a user by a given valid payload', () =>{
+        const payload = {username: "username", id:1, name: "name"}
+        const result = UserView.createUser(payload)
+        expect(result.name).toBe("name")
+        expect(result.username).toBe("username")
+        expect(result.id).toBe(1)
     })
 })
